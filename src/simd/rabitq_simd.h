01777412f88ca2af05a1d8d00131408eb35a2d6f
jingyue.zjl <jingyue.zjl@oceanbase.com>
2025-07-10 14:43:58 +0800

    optimize simd for arm
    Signed-off-by: jingyue.zjl <jingyue.zjl@oceanbase.com>

--- src/simd/rabitq_simd.h
+++ src/simd/rabitq_simd.h
@@ -131,6 +131,18 @@ RotateOp(float* data, int idx, int dim_, int step);
 namespace neon {
 float
 RaBitQFloatBinaryIP(const float* vector, const uint8_t* bits, uint64_t dim, float inv_sqrt_d);
+
+void
+KacsWalk(float* data, std::size_t len);
+
+void
+VecRescale(float* data, std::size_t dim, float val);
+
+void
+FHTRotate(float* data, std::size_t dim_);
+
+void
+RotateOp(float* data, int idx, int dim_, int step);
 }  // namespace neon
 
 using RaBitQFloatBinaryType = float (*)(const float* vector,
